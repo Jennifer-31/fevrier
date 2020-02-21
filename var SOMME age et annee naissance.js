f569ee2e397c650b99063e46642666e282e17fca
Jennifer-31 <57942822+Jennifer-31@users.noreply.github.com>
2020-02-21 14:44:17 +0100

    Add files via upload

---
+++ var SOMME age et annee naissance.js
@@ -0,0 +1,5 @@
+var age = prompt("Entrez votre âge s'il vous plait");
+var naissance = prompt("Entrez votre année de naissance s'il vous plait");
+
+var resultat = age + naissance
+alert("Nous sommes en" + resultat + )
