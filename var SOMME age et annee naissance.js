73f881ca41d1ca609729ab5d4e8294290e573561
Jennifer-31 <57942822+Jennifer-31@users.noreply.github.com>
2020-02-21 15:06:01 +0100

    Delete var SOMME age et annee naissance.js

--- var SOMME age et annee naissance.js
+++
@@ -1,5 +0,0 @@
-var age = prompt("Entrez votre âge s'il vous plait");
-var naissance = prompt("Entrez votre année de naissance s'il vous plait");
-
-var resultat = age + naissance
-alert("Nous sommes en" + resultat + )
